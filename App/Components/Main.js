ee2c961e78a7d3a9136de4b76fbcb84a1e57c2e4
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-06-13 15:38:57 -0700

    Add a Main component that houses the login

---
+++ App/Components/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native'
+
+import Login from "./Login"
+
+class Main extends Component {
+  render(){
+    return (
+      <Login />
+    )
+  }
+}
+
+export default Main
--- index.ios.js
+++ index.ios.js
@@ -12,25 +12,14 @@ import {
   View,
 } from 'react-native';
 
-import Login from './App/Components/Login'
+import Main from './App/Components/Main'
 
 class credit_card_checker_ios extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Login></Login>
-      </View>
+      <Main />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
 
 AppRegistry.registerComponent('credit_card_checker_ios', () => credit_card_checker_ios);
