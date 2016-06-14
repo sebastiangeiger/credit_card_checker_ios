6c0a263601001e3f17dff086a7c4524dd6b15f7a
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-06-14 15:40:02 -0700

    Format the rows

--- App/Components/ExpenseListRow.js
+++ App/Components/ExpenseListRow.js
@@ -9,13 +9,15 @@ class ExpenseListRow extends Component {
   render(){
     let expense = this.props.expense;
     let dayOfMonth = new Date(expense.time_of_sale).getDate();
-    let amountInDollars = expense.amount_in_cents/100.0;
+    let amountInDollars = (expense.amount_in_cents/100.0).toFixed(2);
     return (
       <View style={styles.rowContainer}>
-        <Text>{dayOfMonth}.</Text>
-        <Text>{expense.merchant_name}</Text>
-        <Text>{expense.payment_method_name}</Text>
-        <Text>${amountInDollars}</Text>
+        <Text style={styles.dayOfMonth}>{dayOfMonth}.</Text>
+        <View style={styles.merchantAndPaymentMethod}>
+          <Text>{expense.merchant_name}</Text>
+          <Text>{expense.payment_method_name}</Text>
+        </View>
+        <Text style={styles.amount}>${amountInDollars}</Text>
       </View>
     )
   }
@@ -27,6 +29,20 @@ var styles = StyleSheet.create({
     alignSelf: 'stretch',
     backgroundColor: 'white',
     padding: 10,
+    flexDirection: 'row',
+  },
+  dayOfMonth: {
+    flex: 0.25,
+    fontSize: 30,
+    fontWeight: "100",
+  },
+  merchantAndPaymentMethod: {
+    flex: 1,
+  },
+  amount: {
+    flex: 0.65,
+    fontSize: 30,
+    fontWeight: "100",
   },
 });
 
